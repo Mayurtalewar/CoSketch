93830ca84dff19076550f7b79ea8c1f03758e35a
Mayurtalewar <mayurtalewar@gmail.com>
2024-05-28 00:28:57 +0530

    Make change

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ const parser = require("socket.io-msgpack-parser");
 const CLIENT_URL = process.env.CLIENT_URL;
 const PORT = process.env.PORT || 8080;
 
+console.log("Client URL:", CLIENT_URL);
+console.log("Port:", PORT);
+
 app.use(
   cors({
     origin: [CLIENT_URL],
@@ -26,25 +29,34 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
   socket.on("join", (room) => {
+    console.log(`${socket.id} joined room: ${room}`);
     socket.join(room);
   });
 
   socket.on("leave", (room) => {
+    console.log(`${socket.id} left room: ${room}`);
     socket.leave(room);
   });
 
   socket.on("getElements", ({ elements, room }) => {
+    console.log(`Received elements from ${socket.id} for room ${room}`);
     socket.to(room).emit("setElements", elements);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
   });
 });
 
 app.get("/", (req, res) => {
   res.send(
-    `<marquee>To try the app visite : <a href="${CLIENT_URL}">${CLIENT_URL}</a></marquee>`
+    `<marquee>To try the app visit: <a href="${CLIENT_URL}">${CLIENT_URL}</a></marquee>`
   );
 });
 
 server.listen(PORT, () => {
-  console.log("Listen in port : " + PORT);
+  console.log("Listening on port:", PORT);
 });
